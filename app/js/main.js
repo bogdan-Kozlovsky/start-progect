a718266a802e7dc5dbc8448c5479338c380ac9d5
Bogdan <bogdankozlovski18@gmail.com>
2021-06-03 19:53:19 +0300

    finish

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,31 @@
 $(function () {
+
+  $('.shop-page__number').styler();
+
+  $('.shop-page__slider-carts').slick({
+    asNavFor: '.shop-page__slider-big',
+    arrows: false,
+    vertical: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    focusOnSelect: true,
+    draggable: false,
+  });
+  $('.shop-page__slider-big').slick({
+    asNavFor: '.shop-page__slider-carts',
+    arrows: false,
+    draggable: false,
+    fade: true,
+  });
+  // tabs
+  $('.tabs__link').on('click', function (e) {
+    e.preventDefault();
+    $('.tabs__link').removeClass('tabs__link--active');
+    $(this).addClass('tabs__link--active');
+    $('.tabs-item').removeClass('tabs-item--active');
+    $($(this).attr('href')).addClass('tabs-item--active');
+  });
+  // tabs
   $('.shop-content__filter-btn').on('click', function () {
     $('.shop-content__filter-btn').removeClass('shop-content__filter-btn--active');
     $(this).addClass('shop-content__filter-btn--active');
@@ -22,16 +49,44 @@ $(function () {
       $('.filter-price__to').text(data.to);
     },
   });
-  $('.top-slider__inner').slick({
-    dots: true,
+
+
+  $('.shop-page__slider-carts').slick({
+    asNavFor: '.shop-page__slider-big',
     arrows: false,
-    fade: true,
+    vertical: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    focusOnSelect: true,
+    draggable: false,
   });
+  $('.shop-page__slider-big').slick({
+    asNavFor: '.shop-page__slider-carts',
+    arrows: false,
+    draggable: false,
+  });
+  $('.top-slider__inner').slick({
+    arrows: false,
+    draggable: false,
+    fade: true
+  });
+  $('.blog-standart__slider-list').slick({
+    prevArrow: '<button type="button" class="slick-prev slick-btn"><img src="images/icons/blog-slider-left.svg" alt=""></button>',
+    nextArrow: '<button type="button" class="slick-next slick-btn"><img src="images/icons/blog-slider-right.svg" alt=""></button>',
+  });
+  // звездний рейтинг
   $(".stars").rateYo({
     starWidth: "17px",
     normalFill: "#ccccce",
     ratedFill: "#ffc35b",
   });
+  $(".shop-page__stars, .tabs__info-stars,.related-products__stars").rateYo({
+    starWidth: "17px",
+    normalFill: "#ccccce",
+    ratedFill: "#ffc35b",
+  });
+  // звездний рейтинг
+  // таймер
   function getTimeRemaining(endtime) {
     const total = Date.parse(endtime) - Date.parse(new Date());
     const seconds = Math.floor((total / 1000) % 60);
@@ -72,6 +127,7 @@ $(function () {
   }
   const deadline = $('.time__items').attr('data-time');
   initializeClock('clockdiv', deadline);
+  // таймер
 
 
 });
